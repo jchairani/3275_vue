e76a3e25d94db898b1a5f1a71d8c90f4b6949bff
dnguyen4475 <dnguyen4475@gmail.com>
2022-04-09 17:44:00 -0700

    commit third

--- src/components/AdminFunctions.vue
+++ src/components/AdminFunctions.vue
@@ -1,7 +1,6 @@
 <template>
   <h1>This is the Admin Functions Management Components</h1>
-    <h1>Second coomit</h1>
-
+  <h2>This is third commit</h2>
   <button type="submit" class="badge badge-success" @click="report">
         Generate Report
       </button>
