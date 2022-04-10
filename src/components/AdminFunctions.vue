5ff9ed72226cbc963a145831aed21ea90f14e8b3
jchairani <chairanijoshua@gmail.com>
2022-04-09 18:08:47 -0700

    super last commit

--- src/components/AdminFunctions.vue
+++ src/components/AdminFunctions.vue
@@ -2,6 +2,7 @@
   <h1>This is the Admin Functions Management Components</h1>
   <h2>Last commit</h2>
   <h3>Last Last Commit</h3>
+  <h4>Super last commit</h4>
   <button type="submit" class="badge badge-success" @click="report">
         Generate Report
       </button>
