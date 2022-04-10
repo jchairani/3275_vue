be6a823989be8aaaefff98eb9b654dbfa24fc4f2
dnguyen4475 <dnguyen4475@gmail.com>
2022-04-09 18:06:06 -0700

    Second commit

--- src/components/AdminFunctions.vue
+++ src/components/AdminFunctions.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>This is the Admin Functions Management Components</h1>
   <h2>Last commit</h2>
+  <h3>Last Last Commit</h3>
   <button type="submit" class="badge badge-success" @click="report">
         Generate Report
       </button>
